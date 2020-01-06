701b792bc6d2a66af15339fedc32548c5da2d6b6
Aidan Lincoln <aidantlincoln@gmail.com>
2020-01-06 07:11:47 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,69 @@
 # My Code here....
+def map_to_negativize(source_array)
+  i = 0
+  new_array = []
+  while i < source_array.length do
+    new_array.push(source_array[i] - source_array[i] - source_array[i])
+  i += 1
+  end
+  new_array
+end  
+
+def map_to_no_change(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.length do
+    new_array.push(source_array[i])
+    i += 1
+  end
+  new_array
+end 
+
+def map_to_double(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.length do
+    new_array.push(source_array[i] * 2)
+    i += 1
+  end
+  new_array
+end 
+
+def map_to_square(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.length do
+    new_array.push(source_array[i] * source_array[i])
+    i += 1
+  end
+  new_array
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  value = 0
+  i = 0
+  value += starting_point
+  while i < source_array.length do
+    value += source_array[i]
+    i += 1
+  end
+  value
+end
+  
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.length do
+    if source_array[i] === true
+      boolean = true
+      i += 1
+    else
+      boolean = false
+      break
+    end
+    return boolean
+  end
+end 
+  
+def reduce_to_any_true(source_array)
+  
+end  
